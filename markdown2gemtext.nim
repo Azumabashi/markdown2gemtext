fbdf730022eedaa355dec86a7b31a5c0c87ed98d
Azumabashi <>
2022-11-09 08:35:45 +0900

    feat: implement proc markdown2gemtext

--- markdown2gemtext.nim
+++ markdown2gemtext.nim
@@ -1,2 +1,11 @@
-import markwodn
+import os
+import markdown
 
+proc markdown2gemtext(path: string): string =
+  var file = open(path, FileMode.fmRead)
+  defer:
+    close(file)
+  let html = markdown(file.readAll())
+
+
+discard markdown2gemtext("sample/sample.md")
