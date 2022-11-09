4ecf0c65f5b0e996462e62a5e54a256e9c98138d
Azumabashi <>
2022-11-09 09:28:34 +0900

    reimplement replaceHeaders with replacef

--- markdown2gemtext.nim
+++ markdown2gemtext.nim
@@ -8,17 +8,12 @@ proc replaceHeaders(content: string, level: int): string =
   let
     beginSymbol = fmt"<h{level}>"
     endSymbol = fmt"</h{level}>"
-    matches = content.findAll(re(fmt"{beginsymbol}.*{endSymbol}"))
-  result = content
-  for match in matches:
-    let deletedSymbol = match.replace(beginSymbol, "").replace(endSymbol, "")
-    result = content.replace(match, "*".repeat(level) & " " & deletedSymbol)
+  result = content.replacef(re(fmt"{beginSymbol}(.*){endSymbol}"), "* $1")
 
 proc markdown2gemtext(path: string): string =
   var file = open(path, FileMode.fmRead)
   defer:
     close(file)
   let html = markdown(file.readAll())
-
 
 discard markdown2gemtext("sample/sample.md")
