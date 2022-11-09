ecce6e6dd655f8d71597b94618b40ad801f0c522
Azumabashi <>
2022-11-09 09:56:43 +0900

    feat: implement proc replaceQuotes

--- markdown2gemtext.nim
+++ markdown2gemtext.nim
@@ -15,10 +15,20 @@ proc replaceLists(content: string): string =
   result = content.replacef(re("<li>(.*)</li>"), "* $1")
   result = result.replace("<ul>", "").replace("</ul>", "")
 
+proc replaceQuotes(content: string): string =
+  result = content
+  let quotes = content.findAll(re"<blockquote>(.|\n)*</blockquote>")
+  echo quotes
+  for quote in quotes:
+    var parsed = quote.replacef(re"</?blockquote>\n?", "").replacef(re"<p>((.|\n)*)</p>", "> $1").replace("\n", "\n> ")
+    parsed = parsed[0 ..< parsed.len - 2]  # remove last line of ">\n"
+    result = result.replace(quote, parsed)
+
 proc markdown2gemtext(path: string): string =
   var file = open(path, FileMode.fmRead)
   defer:
     close(file)
   let html = markdown(file.readAll())
+  discard html.replaceQuotes
 
 discard markdown2gemtext("sample/sample.md")
