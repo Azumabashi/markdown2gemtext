84b3a84a97a8c997919bca8719e79114bf2b29fa
Azumabashi <>
2022-11-09 09:24:08 +0900

    feat: implement proc replaceHeaders

--- markdown2gemtext.nim
+++ markdown2gemtext.nim
@@ -1,5 +1,18 @@
 import os
 import markdown
+import re
+import strformat
+import strutils
+
+proc replaceHeaders(content: string, level: int): string =
+  let
+    beginSymbol = fmt"<h{level}>"
+    endSymbol = fmt"</h{level}>"
+    matches = content.findAll(re(fmt"{beginsymbol}.*{endSymbol}"))
+  result = content
+  for match in matches:
+    let deletedSymbol = match.replace(beginSymbol, "").replace(endSymbol, "")
+    result = content.replace(match, "*".repeat(level) & " " & deletedSymbol)
 
 proc markdown2gemtext(path: string): string =
   var file = open(path, FileMode.fmRead)
