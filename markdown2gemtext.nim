71a01e50779a8b8738277f2994d6d5d9492b7784
Azumabashi <>
2022-11-09 09:38:23 +0900

    change leading character of headers

--- markdown2gemtext.nim
+++ markdown2gemtext.nim
@@ -8,7 +8,8 @@ proc replaceHeaders(content: string, level: int): string =
   let
     beginSymbol = fmt"<h{level}>"
     endSymbol = fmt"</h{level}>"
-  result = content.replacef(re(fmt"{beginSymbol}(.*){endSymbol}"), "* $1")
+    leading = "#".repeat(level)
+  result = content.replacef(re(fmt"{beginSymbol}(.*){endSymbol}"), "{leading} $1")
 
 proc replaceLists(content: string): string =
   result = content.replacef(re("<li>(.*)</li>"), "* $1")
